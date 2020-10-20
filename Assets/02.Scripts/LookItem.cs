c946e0f6fa3b4bcfadf9a7e9e5768b7290a1a121
leesungeun07 <63436892+leesungeun07@users.noreply.github.com>
2020-10-20 15:02:58 +0900

    10월 20일

---
+++ Assets/02.Scripts/LookItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LookItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IGvrPointerHoverHandler
+{
+    public void OnLookItemBox(bool isLookAt)
+    {
+        Debug.Log(isLookAt);
+        MoveCtl.isStopped = isLookAt;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        MoveCtl.isStopped = true;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        MoveCtl.isStopped = false;
+    }
+    public void OnGvrPointerHover(PointerEventData eventData)
+    {
+        Debug.Log("Reticle On");
+    }
+}
